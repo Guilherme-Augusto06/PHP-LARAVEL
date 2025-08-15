7c498a6904324b23c365a927d459f1563ebb406d
Guilherme-Augusto06 <ga375464@gmail.com>
2025-08-15 16:20:37 -0300

    feat: Fornecedor

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -6,9 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Middleware\LogAcessoMiddleware;
 class SobreNosController extends Controller
 {
-    public function __construct() {
-        $this->middleware(LogAcessoMiddleware::class);
-    }
+
     public function sobreNos() {
         return view('site.sobre-nos');
     }
---
+++ resources/views/app/fornecedor/listar.blade.php
@@ -0,0 +1,22 @@
+@extends('app.layouts.basico')
+
+@section('titulo', 'Cliente')
+
+@section('conteudo')
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina-2">
+            <p>Fornecedor</p>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="">Novo</a></li>
+                <li><a href="">Consulta</a></li>
+            </ul>
+        </div>
+        <div class="informacao-pagina">
+            <p>Lista</p>
+        </div>
+    </div>
+
+@endsection
+
